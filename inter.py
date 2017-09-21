9a7a4fb0688bcf3a121d932a7bdc5ccccf0be31f
DanielATV <daniel.torov@sansano.usm.cl>
2017-09-21 14:36:34 -0300

    Update inter.py

--- inter.py
+++ inter.py
@@ -11,3 +11,8 @@ If, else if, else, while
 cast
 print
 """
+
+"""
+Funciones > diccionario; llave: nombre; valor: lista(tipo vari entrada,tipo vari salida,sentencias)
+Variabloes > diccionario; llaves: nombre; valor: lista(tipo,valor)
+"""
