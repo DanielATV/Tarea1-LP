e47c74545e0a4c2fd83c8867c34d6c480799bf9e
DanielATV <daniel.torov@sansano.usm.cl>
2017-09-30 14:48:01 -0400

    Update inter.py

--- inter.py
+++ inter.py
@@ -8,14 +8,14 @@ Declaracion de variables
     Variable -> Valor : let mut\s*(\w*)\s*:\s*(i16|i32|f64)\s*=\s*(\d*);
     Variable -> Variable : let mut\s*(\w*)\s*:\s*(i16|i32|f64)\s*=\s*(\w*);
 Funciones: fn\s(\w*)\((\w*):\s(i16|i32|f64)\)\s->\s(i16|i32|f64){ || 
-Retorno
+Retorno: println!\([A-z]+\);
 Operaciones(+ -)
     +/- sin cast -> (\w*|\d*)\s*(\+|\-)\s(\w*|\d*)
     +/- con cast -> \((\w*)\s*as\s*(i16|i32|f64)\)\s*(\+|\-)\s*(\w*) <- cast por la derecha
     +/- con cast -> (\w*)\s*(\+|\-)\s\((\w*)\s*as\s*(i16|i32|f64)\) <- cast por la izquierda
 Boleanos(< <= > >= =)
 If, else if, else, while
-cast \(([A-z][A-z,0-9]*)\sas\s(i16|i32|f64)\)
+Cast: \(([A-z]+) as (i16|i32|f64)\)
 print println!\(([A-z][A-z,0-9]*)\);
 """
 
